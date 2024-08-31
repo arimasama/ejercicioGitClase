dafe0c84c5c4879f443753adc59f11e5c8816347
arimasama <ville2007@hotmail.com>
2024-08-31 09:57:40 -0500

    cambio en ejercicio1 tabla de multiplicar

--- carpeta1/ejercicio1.py
+++ carpeta1/ejercicio1.py
@@ -1,6 +1,6 @@
 def tabla_multiplicar(num):
     for i in range(1, 11):
-        print(f"{num} x {i} = {num * i}")
+        print(f"{num} x {i} = {numero * i}")
 
 numero = int(input("Introduce un número para ver su tabla de multiplicar: "))
 tabla_multiplicar(numero)
