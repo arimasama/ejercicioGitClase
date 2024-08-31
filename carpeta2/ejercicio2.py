4fbec10c0580a83b94da88a35ddf8f13b1e2124c
arimasama <ville2007@hotmail.com>
2024-08-31 09:38:49 -0500

    Cambio de codigo de pares a impares

--- carpeta2/ejercicio2.py
+++ carpeta2/ejercicio2.py
@@ -1,6 +1,6 @@
-def numeros_pares(limite):
-    for i in range(2, limite + 1, 2):
+def numeros_impares(limite):
+    for i in range(1, limite + 1, 2):
         print(i, end=" ")
 
-limite = int(input("Introduce un número para ver los números pares hasta ese número: "))
-numeros_pares(limite)
+limite = int(input("Introduce un número para ver los números impares hasta ese número: "))
+numeros_impares(limite)
